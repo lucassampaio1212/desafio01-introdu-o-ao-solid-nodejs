d2c251a54d30e7a3259d3a471a743418a7314208
lucas <jfontinele9490@gmail.com>
2021-04-02 13:09:21 -0300

    resolução desafio 01 solid

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,23 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+
+  name: string;
+
+  admin: boolean;
+
+  email: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+        this.id = uuid();
+    }
+}
 }
 
 export { User };
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,16 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userEmailExits = this.usersRepository.findByEmail(email);
+
+    if(userEmailExits) {
+      throw new Error("Email already exits");
+    }
+
+    const user = this.usersRepository.create({name, email});
+
+    return user;
+
   }
 }
 
